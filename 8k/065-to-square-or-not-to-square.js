17ba3dd0fa78fde13c51473dcecb548d89fdc8fd
IURII <lytvyn.139@gmail.com>
2020-07-29 21:57:37 -0500

    8k

---
+++ 8k/065-to-square-or-not-to-square.js
@@ -0,0 +1,26 @@
+//https://www.codewars.com/kata/57f6ad55cca6e045d2000627/train/javascript
+
+/* 
+#To square(root) or not to square(root)
+Write a method, that will get an integer array as parameter and will process every number from this array.
+Return a new array with processing every number of the input-array like this:
+
+If the number has an integer square root, take this, otherwise square the number.
+
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+The input array will always contain only positive numbers and will never be empty or null.
+
+The input array should not be modified!
+
+
+*/
+
+function squareOrSquareRoot(array) {
+    return array.map(x => {
+        const n = Math.sqrt(x)
+        return Number.isInteger(n) ? n : x * x
+    })
+}
+
+    // [ 4, 3, 9, 7, 2, 1 ];
+    // [ 2, 9, 3, 49, 4, 1 ];
