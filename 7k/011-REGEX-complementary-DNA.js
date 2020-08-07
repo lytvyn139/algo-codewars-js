1fbbcca1d2c6ec1adaf42d86fa5a230f9057d24f
IURII <lytvyn.139@gmail.com>
2020-08-07 09:47:36 -0500

    7k

--- 7k/011-REGEX-complementary-DNA.js
+++ 7k/011-REGEX-complementary-DNA.js
@@ -1,37 +1,38 @@
 //https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/javascript
-function DNAStrand(dna) {
-    dna = dna.split(' ');
-    console.log(dna);
 
+//this one is actually faster
+function DNAStrand(dna) {
+    dna = dna.split('');
+    let result = []
+    for (let i = 0; i <= dna.length; i++) {
+        if (dna[i] === 'A') {
+            result.push('T');
+        } else if (dna[i] === 'T') {
+            result.push('A');
+        } else if (dna[i] === 'C') {
+            result.push('G');
+        } else if (dna[i] === 'G') {
+            result.push('C');
+        }
+    }
+    return result.join('');
 }
 console.log(DNAStrand('ATTGC'))//TAACG
-// ATTGC
-
-// TAACG //replace A with T || T with A
-//         replace C with G || G with C
 
 
 
 
-// function DNAStrand(dna) {
-//     switch (dna) {
-//         case 'A':
-//             return dna.replace(/[A]/ig, 'T');
-//             break;
-//         case 'T':
-//             return dna.replace(/[T]/ig, 'A');
-//             break;
-//         case 'C':
-//             return dna.replace(/[C]/ig, 'G');
-//             break;
-//         case 'G':
-//             return dna.replace(/[G]/ig, 'C');
-//             break;
-//         default:
-//             console.log('ERROR 404')
-//     }
+function DNAStrand(dna) {
+    return dna.replace(/./g, function (c) {
+        return DNAStrand.pairs[c]
+    })
+}
 
-
-
-
+DNAStrand.pairs = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+}
+console.log(DNAStrand('ATTGC'));
 
