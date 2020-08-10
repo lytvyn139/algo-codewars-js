223e8cd575cebb5961b4b6063a1cba61a9364a76
IURII <lytvyn.139@gmail.com>
2020-08-09 19:21:07 -0500

    7k

---
+++ 7k/024-is-this-triangle.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/56606694ec01347ce800001b/train/javascript
+
+/*
+Implement a method that accepts 3 integer values a, b, c. 
+The method should return true if a triangle can be built with the sides of given length and false in any other case.
+(In this case, all triangles must have surface greater than 0 to be accepted).
+More info:
+*/
+
+function isTriangle(a, b, c) {
+    return a + b > c && a + c > b && c + b > a;
+}
+
