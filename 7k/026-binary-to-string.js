632a74dea49831c3ad17eb089f7f1c713d2bf780
IURII <lytvyn.139@gmail.com>
2020-08-10 22:18:05 -0500

    7k

---
+++ 7k/026-binary-to-string.js
@@ -0,0 +1,9 @@
+////https://www.codewars.com/kata/551f37452ff852b7bd000139/solutions/javascript
+function addBinary(a, b) {
+    const radix = a + b;
+    return radix.toString(2);
+}
+
+function addBinary(a, b) {
+    return (a + b).toString(2)
+}
