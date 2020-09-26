c6d3894d2b1508874a575d9a26b180c337654198
lytvyn139 <lytvyn.139@gmail.com>
2020-09-25 21:02:16 -0500

    7k

--- 7k/056-deodorant-evaporator.js
+++ 7k/056-deodorant-evaporator.js
@@ -1,13 +1,15 @@
 //https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/train/javascript
 function evaporator(content, evap_per_day, threshold){ 
-    let days = 0
-    let percentage = 100.0
-    while (percentage> threshold) {
-        percentage -= percentage * evap_per_day/100;
+    var days = 0;
+    var gas = 100;
+      while(gas >= threshold){
+        gas -= gas * evap_per_day / 100;
         days++;
     }
-    return days;
+      return days;
 }
+evaporator(10,10,10) //22;
+
 // content of the evaporator ml
 // percentage of foam or gas lost every day
 // and the threshold (threshold) in percentage beyond which the evaporator is no longer useful
