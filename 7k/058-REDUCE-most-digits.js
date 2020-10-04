6237c4c851fd4bbf42d25cf3fa3adae752c3caae
lytvyn139 <lytvyn.139@gmail.com>
2020-10-03 19:40:09 -0500

    7k

---
+++ 7k/058-REDUCE-most-digits.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/58daa7617332e59593000006/javascript
+//Find the number with the most digits.
+//If two numbers in the argument array have the same number of digits, return the first one in the array.
+const findLongest = l => l
+  .reduce((a, b) => (`${b}`.length > `${a}`.length) ? b : a);
