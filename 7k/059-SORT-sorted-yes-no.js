abd55be1c54325bec0f6aa1d0b4e52180dff0924
lytvyn139 <lytvyn.139@gmail.com>
2020-10-11 22:06:54 -0500

    7k

---
+++ 7k/059-SORT-sorted-yes-no.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/580a4734d6df748060000045/train/javascript
+const isSortedAndHow = (array) => {
+  const descending = [...array].sort((a, b) => b - a);
+  const ascending = [...array].sort((a, b) => a - b);
+  if (array.every((number, index) => number === descending[index]))
+    return "yes, descending";
+  if (array.every((number, index) => number === ascending[index]))
+    return "yes, ascending";
+  return "no";
+};
